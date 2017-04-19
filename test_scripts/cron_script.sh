cf665ed11558948b738e5d959a88f7a85dbefbce
Jon Rood <jon.rood@nrel.gov>
2017-04-19 17:08:16 -0600

    A little bit of reorganization to nightly testing scripts.

--- test_scripts/cron_script.sh
+++ test_scripts/cron_script.sh
@@ -2,30 +2,7 @@
 
 #Script that runs the nightly tests at NREL on Peregrine
 
-set -e
+NALU_TESTING_DIR=/scratch/jrood/TestNalu
 
-NALU_TESTING_DIR=/scratch/${USER}/TestNalu
-NALU_DIR=${NALU_TESTING_DIR}/Nalu
-export SPACK_ROOT=${NALU_TESTING_DIR}/spack
-
-#Create a test directory if it doesn't exist
-if [ ! -d "${NALU_TESTING_DIR}" ]; then
-  mkdir -p ${NALU_TESTING_DIR}
-
-  #Create and set up nightly directory with Spack installation
-  git clone https://github.com/LLNL/spack.git ${SPACK_ROOT}
-
-  #Configure Spack for Peregrine
-  cd ${NALU_TESTING_DIR} && git clone https://github.com/NaluCFD/NaluSpack.git
-  cd ${NALU_TESTING_DIR}/NaluSpack/spack_config
-  ./copy_config.sh
-
-  #Checkout Nalu and meshes submodule outside of Spack so ctest can build it itself
-  git clone --recursive https://github.com/NaluCFD/Nalu.git ${NALU_DIR}
-
-  #Create a jobs directory
-  mkdir -p ${NALU_TESTING_DIR}/jobs
-fi
-
-#Change to jobs directory and launch tests
 cd ${NALU_TESTING_DIR}/jobs && qsub ${NALU_TESTING_DIR}/NaluSpack/test_scripts/test_nalu_peregrine.sh
+
--- test_scripts/test_nalu_peregrine.sh
+++ test_scripts/test_nalu_peregrine.sh
@@ -10,11 +10,32 @@
 #Script for running regression tests on Peregrine using Spack and submitting results to CDash
 
 #Set nightly directory and Nalu checkout directory
-NALU_TESTING_DIR=/scratch/${USER}/TestNalu
+NALU_TESTING_DIR=/scratch/jrood/TestNalu
 NALU_DIR=${NALU_TESTING_DIR}/Nalu
 
-#Load Spack
+#Set spack location
 export SPACK_ROOT=${NALU_TESTING_DIR}/spack
+
+#Create a test directory if it doesn't exist
+#if [ ! -d "${NALU_TESTING_DIR}" ]; then
+#  mkdir -p ${NALU_TESTING_DIR}
+#
+#  #Create and set up nightly directory with Spack installation
+#  git clone https://github.com/LLNL/spack.git ${SPACK_ROOT}
+#
+#  #Configure Spack for Peregrine
+#  cd ${NALU_TESTING_DIR} && git clone https://github.com/NaluCFD/NaluSpack.git
+#  cd ${NALU_TESTING_DIR}/NaluSpack/spack_config
+#  ./copy_config.sh
+#
+#  #Checkout Nalu and meshes submodule outside of Spack so ctest can build it itself
+#  git clone --recursive https://github.com/NaluCFD/Nalu.git ${NALU_DIR}
+#
+#  #Create a jobs directory
+#  mkdir -p ${NALU_TESTING_DIR}/jobs
+#fi
+
+#Load Spack
 . ${SPACK_ROOT}/share/spack/setup-env.sh
 
 #Test Nalu for trilinos master, develop
@@ -66,3 +87,4 @@ do
     fi 
   done
 done
+

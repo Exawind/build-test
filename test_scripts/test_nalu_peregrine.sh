3000fc56118fa34522e95eabac8ef5cf39781fec
Jon Rood <jon.rood@nrel.gov>
2017-05-03 13:44:00 -0600

    Requesting 64GB node for testing on peregrine.

--- test_scripts/test_nalu_peregrine.sh
+++ test_scripts/test_nalu_peregrine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 #PBS -N test_nalu_peregrine
-#PBS -l nodes=1:ppn=24,walltime=4:00:00
+#PBS -l nodes=1:ppn=24,walltime=4:00:00,feature=64GB
 #PBS -A windFlowModeling
 #PBS -q short
 #PBS -j oe

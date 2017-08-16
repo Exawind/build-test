e919b7052e5bc65f61e9d693b22e7b37c012e77c
Jon Rood <jon.rood@nrel.gov>
2017-08-16 16:21:42 -0600

    Adding torque/moab support to openmpi on Merlin.

--- install_scripts/install_nalu_gcc_merlin.sh
+++ install_scripts/install_nalu_gcc_merlin.sh
@@ -20,7 +20,7 @@ NALUSPACK_ROOT=`pwd`
 # Get general preferred Nalu constraints from a single location
 source ${NALUSPACK_ROOT}/../spack_config/general_preferred_nalu_constraints.sh
 
-MACHINE_SPECIFIC_CONSTRAINTS="^openmpi@1.10.3 fabrics=psm2 ^cmake@3.6.1 ^netlib-lapack"
+MACHINE_SPECIFIC_CONSTRAINTS="^openmpi@1.10.3 fabrics=psm2 schedulers=tm ^cmake@3.6.1 ^netlib-lapack"
 
 ALL_CONSTRAINTS="${GENERAL_CONSTRAINTS} ${MACHINE_SPECIFIC_CONSTRAINTS}"
 

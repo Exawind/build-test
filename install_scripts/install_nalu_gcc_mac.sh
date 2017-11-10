a69f6d32732980b72703bcc1683ebe6d2f94ef69
Jon Rood <jon.rood@nrel.gov>
2017-11-09 17:07:24 -0700

    Adding comments and turning off openmp in osx install script.

--- install_scripts/install_nalu_gcc_mac.sh
+++ install_scripts/install_nalu_gcc_mac.sh
@@ -11,4 +11,7 @@ cmd() {
 # Get general preferred Nalu constraints from a single location
 cmd "source ../spack_config/shared_constraints.sh"
 
+# Disable openmp on osx
+TRILINOS=$(sed 's/+openmp/~openmp/g' <<<"${TRILINOS}")
+
 cmd "spack install nalu %gcc@7.2.0 ^${TRILINOS}@develop ${GENERAL_CONSTRAINTS}"

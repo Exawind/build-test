963b9714a17a6f5b76e4da9d2d7b34cfb750d77b
Jon Rood <jon.rood@nrel.gov>
2017-11-28 09:06:50 -0700

    Loading binutils for intel on peregrine.

--- install_scripts/install_shared_software_peregrine.sh
+++ install_scripts/install_shared_software_peregrine.sh
@@ -109,6 +109,13 @@ do
       printf "\nLoading binutils...\n"
       cmd "spack load binutils %${COMPILER_NAME}@${COMPILER_VERSION}"
     elif [ ${COMPILER_NAME} == 'intel' ]; then
+      # Fix for Peregrine's broken linker for gcc
+      printf "\nInstalling binutils...\n"
+      cmd "spack install binutils %${COMPILER_NAME}@${COMPILER_VERSION}"
+      printf "\nReloading Spack...\n"
+      cmd "source ${SPACK_ROOT}/share/spack/setup-env.sh"
+      printf "\nLoading binutils...\n"
+      cmd "spack load binutils %${COMPILER_NAME}@${COMPILER_VERSION}"
       printf "\nSetting up rpath for Intel...\n"
       # For Intel compiler to include rpath to its own libraries
       for i in ICCCFG ICPCCFG IFORTCFG

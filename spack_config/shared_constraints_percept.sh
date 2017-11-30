b5c2ccb7c29062feadfb6a2fd60c7cf1a94f4208
Jon Rood <jon.rood@nrel.gov>
2017-11-30 13:43:13 -0700

    Adding install script and hacked constraints file for percept.

---
+++ spack_config/shared_constraints_percept.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+TRILINOS="trilinos~alloptpkgs~xsdkflags~metis~mumps~superlu-dist+superlu~hypre+hdf5~suite-sparse~python~shared+boost+tpetra+epetra+epetraext+exodus+pnetcdf+zlib+stk+teuchos+belos+zoltan+zoltan2~amesos+amesos2~anasazi~ifpack+ifpack2+muelu~fortran~ml+gtest+aztec+sacado~x11+instantiate~instantiate_cmplx~dtk~fortrilinos~openmp+shards~nox+intrepid~intrepid2+cgns"
+
+GENERAL_CONSTRAINTS="^boost+filesystem+graph+mpi+program_options+regex+serialization+signals+system+thread@1.60.0 ^parallel-netcdf@1.6.1 ^hdf5@1.8.16 ^netcdf@4.3.3.1 ^superlu@4.3"

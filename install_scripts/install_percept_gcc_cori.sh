38b75004f57872e869af79c09533c5c27173b5cf
Jon Rood <jsrood@lbl.gov>
2017-12-04 09:49:12 -0800

    Adding percept scripts for cori.

---
+++ install_scripts/install_percept_gcc_cori.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#PBS -N percept_build_gcc
+#PBS -l nodes=1:ppn=24,walltime=1:00:00
+#PBS -A windsim
+#PBS -q short
+#PBS -j oe
+#PBS -W umask=002
+
+#Script for installing Nalu on Peregrine using Spack with GCC compiler
+
+# Function for printing and executing commands
+cmd() {
+  echo "+ $@"
+  eval "$@"
+}
+
+set -e
+
+cmd "module purge"
+cmd "module load gcc/5.2.0"
+cmd "module load python/2.7.8 &> /dev/null"
+cmd "module unload mkl"
+
+# Get general preferred Nalu constraints from a single location
+cmd "source ../spack_config/shared_constraints_percept.sh"
+
+# Fix for Peregrine's broken linker
+cmd "spack install binutils %gcc@5.2.0"
+cmd "source ${SPACK_ROOT}/share/spack/setup-env.sh"
+cmd "spack load binutils"
+
+# Sometimes /tmp runs out of space for some reason so set TMPDIR to /scratch
+cmd "mkdir -p /scratch/${USER}/.tmp"
+cmd "export TMPDIR=/scratch/${USER}/.tmp"
+
+cmd "spack install percept %gcc@5.2.0 ^${TRILINOS}@develop ${GENERAL_CONSTRAINTS}"
---
+++ install_scripts/install_shared_percept_cori.sh
@@ -0,0 +1,96 @@
+#!/bin/bash -l
+
+##PBS -N install_shared_percept_peregrine
+##PBS -l nodes=1:ppn=24,walltime=4:00:00,feature=haswell
+##PBS -A windsim
+##PBS -q short
+##PBS -j oe
+##PBS -W umask=002
+
+# Control over printing and executing commands
+print_cmds=true
+execute_cmds=true
+
+# Function for printing and executing commands
+cmd() {
+  if ${print_cmds}; then echo "+ $@"; fi
+  if ${execute_cmds}; then eval "$@"; fi
+}
+
+printf "============================================================\n"
+printf "$(date)\n"
+printf "============================================================\n"
+printf "Job is running on ${HOSTNAME}\n"
+printf "============================================================\n"
+
+# Set some version numbers
+GCC_COMPILER_VERSION="6.3.0"
+INTEL_COMPILER_VERSION="17.0.2"
+
+# Set installation directory
+INSTALL_DIR=$SCRATCH/percept
+NALUSPACK_DIR=${INSTALL_DIR}/NaluSpack
+
+# Set spack location
+export SPACK_ROOT=${INSTALL_DIR}/spack
+
+if [ ! -d "${INSTALL_DIR}" ]; then
+  printf "============================================================\n"
+  printf "Install directory doesn't exist.\n"
+  printf "Creating everything from scratch...\n"
+  printf "============================================================\n"
+
+  printf "Creating top level install directory...\n"
+  cmd "mkdir -p ${INSTALL_DIR}"
+
+  printf "\nCloning Spack repo...\n"
+  cmd "git clone https://github.com/jrood-nrel/spack.git ${SPACK_ROOT}"
+  cmd "cd ${SPACK_ROOT} && git checkout add_percept"
+
+  printf "\nConfiguring Spack...\n"
+  cmd "git clone https://github.com/NaluCFD/NaluSpack.git ${NALUSPACK_DIR}"
+  cmd "cd ${NALUSPACK_DIR}/spack_config && ./setup_spack.sh"
+
+  printf "============================================================\n"
+  printf "Done setting up install directory.\n"
+  printf "============================================================\n"
+fi
+
+printf "\nLoading Spack...\n"
+cmd "source ${SPACK_ROOT}/share/spack/setup-env.sh"
+
+for TRILINOS_BRANCH in develop
+do
+  for COMPILER_NAME in gcc
+  do
+    if [ ${COMPILER_NAME} == 'gcc' ]; then
+      COMPILER_VERSION="${GCC_COMPILER_VERSION}"
+    elif [ ${COMPILER_NAME} == 'intel' ]; then
+      COMPILER_VERSION="${INTEL_COMPILER_VERSION}"
+    fi
+    printf "\nInstalling software with ${COMPILER_NAME}@${COMPILER_VERSION} at $(date).\n"
+
+    unset GENERAL_CONSTRAINTS
+    cmd "source ${INSTALL_DIR}/NaluSpack/spack_config/shared_constraints_percept_cori.sh"
+    printf "\nUsing constraints: ${GENERAL_CONSTRAINTS}\n"
+
+    cd ${INSTALL_DIR}
+
+    printf "\nInstalling Percept using ${COMPILER_NAME}@${COMPILER_VERSION}...\n"
+    if [ ${COMPILER_NAME} == 'gcc' ]; then
+      cmd "spack install percept %${COMPILER_NAME}@${COMPILER_VERSION} ^${TRILINOS}@${TRILINOS_BRANCH} ${GENERAL_CONSTRAINTS}"
+    fi
+
+    printf "\nDone installing shared software with ${COMPILER_NAME}@${COMPILER_VERSION} at $(date).\n"
+  done
+done
+
+#printf "\nSetting permissions...\n"
+#cmd "chmod -R a+rX,go-w ${INSTALL_DIR}"
+#cmd "chmod g+w ${INSTALL_DIR}"
+#cmd "chmod g+w ${INSTALL_DIR}/spack"
+#cmd "chmod g+w ${INSTALL_DIR}/spack/opt"
+#cmd "chmod g+w ${INSTALL_DIR}/spack/opt/spack"
+#cmd "chmod -R g+w ${INSTALL_DIR}/spack/opt/spack/.spack-db"
+printf "\n$(date)\n"
+printf "\nDone!\n"

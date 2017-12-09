1b2693e57aa55d1bee0f0c1bbf79b5ee945f375b
Jon Rood <jon.rood@nrel.gov>
2017-12-08 18:24:44 -0700

    Adding custom nalu package file with catalyst option.

--- spack_config/custom_package_files/catalyst-ioss-adapter/package.py
+++ spack_config/custom_package_files/catalyst-ioss-adapter/package.py
@@ -7,8 +7,8 @@ class CatalystIossAdapter(CMakePackage):
     homepage = "https://github.com/nalucfd/naluspack"
     url      = "https://github.com/nalucfd/naluspack.git"
 
-    version('develop', '59163fd085a24c7a4c2170c70bb60fea',
-            url='https://github.com/nalucfd/naluspack/raw/master/spack_config/custom_package_files/catalyst-ioss-adapter/ParaviewCatalystIossAdapter.tar.gz')
+    version('develop', 'aa5266fddb8554d39c6087550d3c8b27',
+            url='https://github.com/NaluCFD/NaluSpack/raw/master/spack_config/custom_package_files/catalyst-ioss-adapter/ParaViewCatalystIossAdapter.tar.gz')
 
     depends_on('paraview+mpi+python+osmesa')
 

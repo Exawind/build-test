d217b807cd56fe6f402e897955be8622a05f1def
Jon Rood <jon.rood@nrel.gov>
2018-02-15 09:52:49 -0700

    Forcing yaml-cpp@develop in custom mira nalu package file.

--- spack_config/machines/mira/nalu/package.py
+++ spack_config/machines/mira/nalu/package.py
@@ -48,7 +48,7 @@ class Nalu(CMakePackage):
             git='https://github.com/NaluCFD/Nalu.git', branch='master')
 
     # Currently Nalu only builds with certain libraries statically
-    depends_on('yaml-cpp+pic~shared@0.5.3:')
+    depends_on('yaml-cpp+pic~shared@develop')
     depends_on('trilinos~shared+exodus+tpetra+muelu+belos+ifpack2+amesos2+zoltan+stk+boost~superlu-dist+superlu+hdf5+zlib+pnetcdf+shards@master,12.12.1:')
     depends_on('openfast+cxx', when='+openfast')
     depends_on('tioga', when='+tioga')

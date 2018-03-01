0ae4e93d36025d354f2c2522bf645f1f5484faa6
Jon Rood <jon.rood@nrel.gov>
2018-03-01 14:22:47 -0700

    Adding install script for clang on mac.

---
+++ install_scripts/install_nalu_clang_mac.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Script for installing Nalu on a Mac using Spack with Apple's Clang compiler
+
+# Function for printing and executing commands
+cmd() {
+  echo "+ $@"
+  eval "$@"
+}
+
+# Get general preferred Nalu constraints from a single location
+cmd "source ../spack_config/shared_constraints.sh"
+
+# Disable openmp on osx
+TRILINOS=$(sed 's/+openmp/~openmp/g' <<<"${TRILINOS}")
+
+cmd "spack install nalu+openfast+tioga+hypre %clang@9.0.0-apple ^${TRILINOS}@develop"

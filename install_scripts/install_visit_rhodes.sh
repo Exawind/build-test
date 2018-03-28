82802bc42506a32bc99f5a0663cc93038316fc3e
Jon Rood <jon.rood@nrel.gov>
2018-03-28 16:18:58 -0600

    More additions to visit script on rhodes.

--- install_scripts/install_visit_rhodes.sh
+++ install_scripts/install_visit_rhodes.sh
@@ -9,6 +9,7 @@ set -e
 
 VISIT_DIR=${HOME}/visit
 
+# Load the necessary modules
 cmd "module purge"
 cmd "module load unzip"
 cmd "module load patch"
@@ -24,4 +25,8 @@ cmd "module load libxml2/2.9.4"
 cmd "module load makedepend/1.0.5"
 cmd "module list"
 
+# Setup the directory and run the build visit script
 cmd "mkdir -p ${VISIT_DIR} && cp build_visit2_13_0 ${VISIT_DIR}/ && cd ${VISIT_DIR} && ./build_visit2_13_0 --makeflags -j32 --parallel --required --optional --all-io --nonio --no-fastbit --no-fastquery --prefix ${VISIT_DIR}/install"
+
+# Set the permissions
+#cmd "chmod -R a+rX,go-w ${VISIT_DIR}"

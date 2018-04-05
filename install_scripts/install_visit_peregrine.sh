34ce9f24062db3adf274b9b180f09df039e0ee67
Jon Rood <jon.rood@nrel.gov>
2018-04-04 20:53:02 -0600

    Updating visit install script on peregrine and adding script to set permissions on shared software.

--- install_scripts/install_visit_peregrine.sh
+++ install_scripts/install_visit_peregrine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 #PBS -N build_visit
-#PBS -l nodes=1:ppn=24,walltime=4:00:00
+#PBS -l nodes=1:ppn=24,walltime=4:00:00,feature=haswell
 #PBS -A windsim
 #PBS -q short
 #PBS -j oe
@@ -14,15 +14,11 @@ cmd() {
 
 set -e
 
-#export SPACK_ROOT=${HOME}/spack
-#source ${SPACK_ROOT}/share/spack/setup-env.sh
 cmd "module purge"
-cmd "module use /nopt/nrel/apps/modules/candidate/modulefiles"
 cmd "module use /projects/windsim/exawind/BaseSoftware/spack/share/spack/modules/linux-centos6-x86_64"
 cmd "module load gcc/5.2.0"
-#cmd "module load openmpi-gcc/1.10.0-5.2.0"
 cmd "module load python/2.7.14"
-cmd "module load git/2.6.3"
+cmd "module load git/2.14.1"
 cmd "module load binutils/2.28"
 cmd "module load libxml2/2.9.4"
 cmd "module load makedepend/1.0.5"
@@ -31,7 +27,7 @@ cmd "module list"
 cmd "mkdir -p /scratch/${USER}/.tmp"
 cmd "export TMPDIR=/scratch/${USER}/.tmp"
 
-cmd "cd /projects/windsim/exawind/software/visit/a"
 #cmd "export PAR_COMPILER=mpicc; export PAR_COMPILER_CXX=mpicxx; export PAR_INCLUDE=/nopt/nrel/apps/openmpi/1.10.0-serial-gcc-5.2.0/include; export PAR_LIBS=/nopt/nrel/apps/openmpi/1.10.0-serial-gcc-5.2.0/lib/libmpi.a; ./build_visit2_13_0 --makeflags -j24 --parallel --required --optional --all-io --nonio --no-fastbit --no-fastquery --prefix /projects/windsim/exawind/software/visit/a/install"
-# Using its own builtin mpich
-cmd "./build_visit2_13_0 --makeflags -j24 --parallel --required --optional --all-io --nonio --no-fastbit --no-fastquery --prefix /projects/windsim/exawind/software/visit/a/install"
+
+VISIT_DIR=/nopt/nrel/ecom/ecp/base/a/visit
+cmd "cp build_visit2_13_0 ${VISIT_DIR}/ && cd ${VISIT_DIR} && ./build_visit2_13_0 --makeflags -j24 --parallel --required --optional --all-io --nonio --no-fastbit --no-fastquery --prefix ${VISIT_DIR}/install"
---
+++ spack_config/machines/peregrine/fix_permissions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+print_cmds=true
+execute_cmds=true
+
+cmd() {
+  if ${print_cmds}; then echo "+ $@"; fi
+  if ${execute_cmds}; then eval "$@"; fi
+}
+
+cmd "chmod a+rX,o-w,ug+w /nopt/nrel/ecom/ecp"

87057ebeb2fd20d83d776baf39df85e2861ec2d6
Jon Rood <jon.rood@nrel.gov>
2018-05-01 14:05:33 -0600

    Updating symlink update script for rhodes.

--- spack_config/machines/rhodes/update_links_rhodes.sh
+++ spack_config/machines/rhodes/update_links_rhodes.sh
@@ -2,9 +2,4 @@
 
 DIR=a
 
-unlink active
-unlink modules
-unlink module_prefix
-ln -s ${DIR} active
-ln -s ${DIR}/spack/share/spack/modules/linux-centos7-x86_64 modules
-ln -s ${DIR}/spack/opt/spack/linux-centos7-x86_64/gcc-4.8.5/environment-modules-3.2.10-3x6hrfov45yzzquhieonp4acxkhcrvhh module_prefix
+cd /opt/software && unlink active && unlink modules && unlink module_prefix && ln -s ${DIR} active && ln -s ${DIR}/spack/share/spack/modules/linux-centos7-x86_64 modules && ln -s ${DIR}/spack/opt/spack/linux-centos7-x86_64/gcc-4.8.5/environment-modules-3.2.10-3x6hrfov45yzzquhieonp4acxkhcrvhh module_prefix

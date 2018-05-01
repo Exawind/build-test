a2a924862877b0bdb0b71335fca62a0ba220e664
Jon Rood <jon.rood@nrel.gov>
2018-05-01 11:41:14 -0600

    Updating example dot file for peregrine.

--- spack_config/machines/peregrine/dot_bash_profile_peregrine.sh
+++ spack_config/machines/peregrine/dot_bash_profile_peregrine.sh
@@ -9,13 +9,12 @@ fi
 
 # User specific environment and startup programs
 
-{
 module purge
-module load gcc/5.2.0
-module load python/2.7.8
-module unload mkl
-} &> /dev/null
+module use /nopt/nrel/ecom/ecp/base/modules/gcc-6.2.0
+#module use /nopt/nrel/ecom/ecp/base/modules/intel-18.1.163
+module load gcc/6.2.0
+module load git/2.15.1
+module load python/2.7.14
 
 export SPACK_ROOT=${HOME}/spack
 . ${SPACK_ROOT}/share/spack/setup-env.sh
-

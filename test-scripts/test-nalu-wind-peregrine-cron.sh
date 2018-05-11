262e25dc3bf8b5f3e81c60dc14eeca68b4ba4c15
Jon Rood <jon.rood@nrel.gov>
2018-05-11 14:58:01 -0600

    Use batch-h queue for peregrine testing.

--- test-scripts/test-nalu-wind-peregrine-cron.sh
+++ test-scripts/test-nalu-wind-peregrine-cron.sh
@@ -7,7 +7,7 @@ qsub \
 -N test-nalu-wind \
 -l nodes=1:ppn=24,walltime=4:00:00,feature=haswell \
 -A windsim \
--q short \
+-q batch-h \
 -j oe \
 -W umask=002 \
 /projects/windsim/exawind/nalu-wind-testing/build-test/test-scripts/test-nalu-wind-peregrine.sh

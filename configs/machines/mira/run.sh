661813473d84396937cb3ac7c36ee8c1a4439b97
Jon Rood <jon.rood@nrel.gov>
2018-05-25 22:20:01 +0000

    Updating run script for mira.

--- configs/machines/mira/run.sh
+++ configs/machines/mira/run.sh
@@ -3,4 +3,4 @@
 # Script for running nalu job on Mira
 #qsub -A ExaWindFarm -t 30 -n 1 --mode script run.sh
 
-cd /home/${USER}/nalu/build/reg_tests/test_files/ablNeutralEdge && runjob --np 8 -p 16 --envs HDF5_DISABLE_VERSION_CHECK=2 --block $COBALT_PARTNAME --verbose=INFO : /home/${USER}/nalu/build/naluX -i /home/${USER}/nalu/reg_tests/test_files/ablNeutralEdge/ablNeutralEdge.i -o /home/${USER}/nalu/build/reg_tests/test_files/ablNeutralEdge/ablNeutralEdge.log
+cd /home/${USER}/Nalu/build/reg_tests/test_files/ablNeutralEdge && runjob --np 8 -p 16 --envs HDF5_DISABLE_VERSION_CHECK=2 --block $COBALT_PARTNAME --verbose=INFO : /home/${USER}/Nalu/build/naluX -i /home/${USER}/Nalu/reg_tests/test_files/ablNeutralEdge/ablNeutralEdge.i -o /home/${USER}/Nalu/build/reg_tests/test_files/ablNeutralEdge/ablNeutralEdge.log

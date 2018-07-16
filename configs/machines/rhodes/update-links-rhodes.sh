0375a3d9b41e8c52137222b60c9d05244441c659
Jon Rood <jon.rood@nrel.gov>
2018-07-16 10:33:49 -0600

    Updating packages.yaml for rhodes.

--- configs/machines/rhodes/update-links-rhodes.sh
+++ configs/machines/rhodes/update-links-rhodes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DIR=b
+DIR=a
 
 cd /opt/software && unlink active && unlink modules && unlink module_prefix && ln -s ${DIR} active && ln -s ${DIR}/spack/share/spack/modules/linux-centos7-x86_64/gcc-4.8.5 modules && ln -s ${DIR}/spack/opt/spack/linux-centos7-x86_64/gcc-4.8.5/environment-modules-3.2.10-c5xiwsdznporezjx7cgtvzsh3as576d3 module_prefix

30f9e2737b03c8f976ffadfc55dc2c99c719928d
Jon Rood <jon.rood@nrel.gov>
2018-07-17 21:46:58 -0600

    Updating peregrine visit script.

--- install-scripts/install_visit_peregrine.sh
+++ install-scripts/install_visit_peregrine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 #PBS -N build_visit
-#PBS -l nodes=1:ppn=24,walltime=6:00:00
+#PBS -l nodes=1:ppn=24,walltime=6:00:00,feature=haswell
 #PBS -A windsim
 #PBS -q batch-h
 #PBS -j oe
@@ -15,28 +15,26 @@ cmd() {
 set -e
 
 cmd "module purge"
-cmd "module use /nopt/nrel/ecom/ecp/base/modules/gcc-6.2.0"
-cmd "module load gcc/5.5.0"
-cmd "module load python/2.7.14"
-cmd "module load git/2.17.0"
-cmd "module load binutils/2.29.1"
-cmd "module load libxml2/2.9.4-py2"
-cmd "module load makedepend/1.0.5"
-cmd "module load netlib-lapack/3.8.0"
+cmd "module use /nopt/nrel/ecom/ecp/base/a/spack/share/spack/modules/linux-centos7-x86_64/gcc-6.2.0"
+cmd "module load gcc/6.2.0"
+cmd "module load python/2.7.15"
+cmd "module load git/2.17.1"
+cmd "module load binutils"
+cmd "module load libxml2"
+cmd "module load makedepend"
+cmd "module load netlib-lapack"
 cmd "module list"
 
 cmd "mkdir -p /scratch/${USER}/.tmp"
 cmd "export TMPDIR=/scratch/${USER}/.tmp"
 
-#cmd "export PAR_COMPILER=mpicc; export PAR_COMPILER_CXX=mpicxx; export PAR_INCLUDE=/nopt/nrel/apps/openmpi/1.10.0-serial-gcc-5.2.0/include; export PAR_LIBS=/nopt/nrel/apps/openmpi/1.10.0-serial-gcc-5.2.0/lib/libmpi.a; ./build_visit2_13_0 --makeflags -j24 --parallel --required --optional --all-io --nonio --no-fastbit --no-fastquery --prefix /projects/windsim/exawind/software/visit/a/install"
+export MY_VISIT_DIR=/nopt/nrel/ecom/ecp/base/a/visit
+export MY_PYTHON_LIB=${MY_VISIT_DIR}/visit/python/2.7.11/linux-x86_64_gcc-4.8/lib/libpython2.7.so
 
-VISIT_DIR=/nopt/nrel/ecom/ecp/base/c/visit
-cmd "cp build_visit2_13_0 ${VISIT_DIR}/ && cd ${VISIT_DIR} && ./build_visit2_13_0 --makeflags -j24 --parallel --required --optional --all-io --nonio --no-fastbit --no-fastquery --prefix ${VISIT_DIR}/install"
+cmd "mkdir -p ${MY_VISIT_DIR} && cp build_visit2_13_2 ${MY_VISIT_DIR}/ && cd ${MY_VISIT_DIR} && ./build_visit2_13_2 --makeflags -j24 --parallel --required --optional --all-io --nonio --no-fastbit --no-fastquery --prefix ${MY_VISIT_DIR}/install"
 
-#Then manually create module file for visit
-
-#Then add this to /nopt/nrel/ecom/ecp/base/c/visit/install/2.13.0/bin/internallauncher
-#SETENV("PATH", self.joinpaths(["/nopt/nrel/ecom/ecp/base/c/visit/install/bin", "/nopt/nrel/ecom/ecp/base/c/visit/install/2.13.0/linux-x86_64/bin", GETENV("PATH")]))
-#SETENV("LD_LIBRARY_PATH", self.joinpaths(["/nopt/nrel/ecom/ecp/base/c/visit/install/2.13.0/linux-x86_64/lib", GETENV("LD_LIBRARY_PATH")]))
-#SETENV("LIBRARY_PATH", self.joinpaths(["/nopt/nrel/ecom/ecp/base/c/visit/install/2.13.0/linux-x86_64/lib", GETENV("LIBRARY_PATH")]))
-#SETENV("VISIT_ROOT_DIR", "/nopt/nrel/ecom/ecp/base/c/visit/install")
+#Then add this to /nopt/nrel/ecom/ecp/base/a/visit/install/2.13.0/bin/internallauncher
+#SETENV("PATH", self.joinpaths(["/nopt/nrel/ecom/ecp/base/a/visit/install/bin", "/nopt/nrel/ecom/ecp/base/a/visit/install/2.13.0/linux-x86_64/bin", GETENV("PATH")]))
+#SETENV("LD_LIBRARY_PATH", self.joinpaths(["/nopt/nrel/ecom/ecp/base/a/visit/install/2.13.0/linux-x86_64/lib", GETENV("LD_LIBRARY_PATH")]))
+#SETENV("LIBRARY_PATH", self.joinpaths(["/nopt/nrel/ecom/ecp/base/a/visit/install/2.13.0/linux-x86_64/lib", GETENV("LIBRARY_PATH")]))
+#SETENV("VISIT_ROOT_DIR", "/nopt/nrel/ecom/ecp/base/a/visit/install")

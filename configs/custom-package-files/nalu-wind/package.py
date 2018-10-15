1dd00f31329d13105703e659b1c7823f6819299f
Jon Rood <jon.rood@nrel.gov>
2018-10-15 13:04:41 -0600

    Updating custom nalu-wind package file that includes catalyst.

--- configs/custom-package-files/nalu-wind/package.py
+++ configs/custom-package-files/nalu-wind/package.py
@@ -22,15 +22,20 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
+import sys
 
 
 class NaluWind(CMakePackage):
     """Nalu-Wind: Wind energy focused variant of Nalu."""
 
     homepage = "https://github.com/exawind/nalu-wind"
-    url      = "https://github.com/exawind/nalu-wind.git"
+    git      = "https://github.com/exawind/nalu-wind.git"
 
     maintainers = ['jrood-nrel']
+
+    tags = ['ecp', 'ecp-apps']
+
+    version('master', branch='master')
 
     variant('openfast', default=False,
             description='Compile with OpenFAST support')
@@ -38,15 +43,17 @@ class NaluWind(CMakePackage):
             description='Compile with Tioga support')
     variant('hypre', default=False,
             description='Compile with Hypre support')
+    variant('shared', default=(sys.platform != 'darwin'),
+            description='Build Trilinos as shared library')
     variant('catalyst', default=False,
             description='Compile with Catalyst support')
 
-    version('master',
-            git='https://github.com/exawind/nalu-wind.git', branch='master')
-
     depends_on('mpi')
     depends_on('yaml-cpp@0.5.3:')
-    depends_on('trilinos+exodus+tpetra+muelu+belos+ifpack2+amesos2+zoltan+stk+boost~superlu-dist+superlu+hdf5+zlib+pnetcdf+shards~hypre@master,develop')
+    depends_on('trilinos+exodus+tpetra+muelu+belos+ifpack2+amesos2+zoltan+stk+boost~superlu-dist+superlu+hdf5+zlib+pnetcdf+shards~hypre@master,develop', when='+shared')
+    # Cannot build Trilinos as a shared library with STK on Darwin
+    # https://github.com/trilinos/Trilinos/issues/2994
+    depends_on('trilinos~shared+exodus+tpetra+muelu+belos+ifpack2+amesos2+zoltan+stk+boost~superlu-dist+superlu+hdf5+zlib+pnetcdf+shards~hypre@master,develop', when='~shared')
     depends_on('openfast+cxx', when='+openfast')
     depends_on('tioga', when='+tioga')
     depends_on('hypre+mpi+int64', when='+hypre')

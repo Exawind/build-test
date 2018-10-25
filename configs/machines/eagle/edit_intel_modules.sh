755bd59ef5c871ea8c9c3e5e61ea0dab65c33e8a
Jon Rood <jon.rood@nrel.gov>
2018-10-24 20:33:02 -0600

    Updating eagle stuff.

--- configs/machines/eagle/edit_intel_modules.sh
+++ configs/machines/eagle/edit_intel_modules.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-DIR=/scratch/jrood/eagle/eagle_compilers/spack/share/spack/modules/linux-centos7-x86_64
+DIR=/scratch/jrood/eagle2/eagle_compilers/spack/share/spack/modules/linux-centos7-x86_64
 
 for file_name in cluster.2019.0 cluster.2018.3 cluster.2017.7; do
   find ${DIR} -name "${file_name}" -type f | xargs sed -i -e '/INTEL_LICENSE_FILE/d'

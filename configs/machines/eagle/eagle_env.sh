2b5f6f1ad3cba9a2ec43dc3683e2e72ff3973c9e
Jon Rood <jon.rood@nrel.gov>
2018-11-09 14:03:24 -0700

    Updates for Eagle in preparation for installing Nalu-Wind.

--- configs/machines/eagle/eagle_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-
-module purge
-module unuse /nopt/nrel/apps/modules/centos7/modulefiles
-module unuse /nopt/nrel/ecom/ecp/base/modules/gcc-6.2.0
-module use /scratch/jrood/eagle2/eagle_software/spack/share/spack/modules/linux-centos7-x86_64
-module use /scratch/jrood/eagle2/eagle_compilers/spack/share/spack/modules/linux-centos7-x86_64/gcc-6.2.0
-module purge

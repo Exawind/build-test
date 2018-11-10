4bb753aa932328d6ac32dd0737a00dfb4f3c646e
Jon Rood <jon.rood@nrel.gov>
2018-11-09 18:20:19 -0700

    Adding eagle do-config and example slurm script.

---
+++ configs/machines/eagle/do-config-nalu-wind-eagle.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Instructions:
+# Make a directory in the Nalu-Wind directory for building,
+# Copy this script to that directory and edit the
+# options below to your own needs and run it.
+
+COMPILER=gcc
+
+if [ "${COMPILER}" == 'gcc' ]; then
+  CXX_COMPILER=mpicxx
+  C_COMPILER=mpicc
+  FORTRAN_COMPILER=mpifort
+fi
+  
+set -e
+
+cmd() {
+  echo "+ $@"
+  eval "$@"
+}
+
+# Set up environment on Eagle
+cmd "module purge"
+if [ "${COMPILER}" == 'gcc' ]; then
+  cmd "module use /nopt/nrel/ecom/hpacf/2018-11-09/spack/share/spack/modules/linux-centos7-x86_64/gcc-7.3.0"
+  cmd "module load git"
+  cmd "module load python"
+  cmd "module load openmpi"
+  cmd "module load netlib-lapack"
+fi
+cmd "module load gcc/7.3.0"
+cmd "module load openfast"
+cmd "module load hypre"
+cmd "module load tioga"
+cmd "module load yaml-cpp"
+cmd "module load cmake"
+cmd "module load trilinos"
+cmd "module list"
+
+# Set tmpdir to the scratch filesystem so it doesn't run out of space
+cmd "mkdir -p ${HOME}/.tmp"
+cmd "export TMPDIR=${HOME}/.tmp"
+
+# Clean before cmake configure
+set +e
+cmd "rm -rf CMakeFiles"
+cmd "rm -f CMakeCache.txt"
+set -e
+
+cmd "which cmake"
+cmd "which mpirun"
+
+(set -x; cmake \
+  -DCMAKE_CXX_COMPILER:STRING=${CXX_COMPILER} \
+  -DCMAKE_C_COMPILER:STRING=${C_COMPILER} \
+  -DCMAKE_Fortran_COMPILER:STRING=${FORTRAN_COMPILER} \
+  -DMPI_CXX_COMPILER:STRING=${CXX_COMPILER} \
+  -DMPI_C_COMPILER:STRING=${C_COMPILER} \
+  -DMPI_Fortran_COMPILER:STRING=${FORTRAN_COMPILER} \
+  -DTrilinos_DIR:PATH=${TRILINOS_ROOT_DIR} \
+  -DYAML_DIR:PATH=${YAML_CPP_ROOT_DIR} \
+  -DENABLE_OPENFAST:BOOL=ON \
+  -DOpenFAST_DIR:PATH=${OPENFAST_ROOT_DIR} \
+  -DENABLE_HYPRE:BOOL=ON \
+  -DHYPRE_DIR:PATH=${HYPRE_ROOT_DIR} \
+  -DENABLE_TIOGA:BOOL=ON \
+  -DTIOGA_DIR:PATH=${TIOGA_ROOT_DIR} \
+  -DCMAKE_BUILD_TYPE:STRING=RELEASE \
+  -DENABLE_DOCUMENTATION:BOOL=OFF \
+  -DENABLE_TESTS:BOOL=ON \
+  -DCMAKE_SKIP_BUILD_RPATH:BOOL=FALSE \
+  -DCMAKE_BUILD_WITH_INSTALL_RPATH:BOOL=FALSE \
+  -DCMAKE_INSTALL_RPATH_USE_LINK_PATH:BOOL=TRUE \
+  -DCMAKE_BUILD_RPATH:STRING="${NETLIB_LAPACK_ROOT_DIR}/lib64;${TIOGA_ROOT_DIR}/lib;${HYPRE_ROOT_DIR}/lib;${OPENFAST_ROOT_DIR}/lib;${YAML_ROOT_DIR}/lib;${TRILINOS_ROOT_DIR}/lib;$(pwd)" \
+  ..)
+
+cmd "nice make -j 20"
---
+++ configs/machines/eagle/test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+
+#SBATCH -J nalu-wind-test
+#SBATCH -o %x.o%j
+#SBATCH -t 30
+#SBATCH -N 1
+
+# To run use the command `sbatch test.sh`
+ 
+set -e
+
+cmd() {
+  echo "+ $@"
+  eval "$@"
+}
+
+# Set up environment on Eagle
+cmd "module purge"
+cmd "module use /nopt/nrel/ecom/hpacf/2018-11-09/spack/share/spack/modules/linux-centos7-x86_64/gcc-7.3.0"
+cmd "module load git"
+cmd "module load python"
+cmd "module load openmpi"
+cmd "module load netlib-lapack"
+cmd "module load gcc/7.3.0"
+cmd "module load openfast"
+cmd "module load hypre"
+cmd "module load tioga"
+cmd "module load yaml-cpp"
+cmd "module load cmake"
+cmd "module load trilinos"
+cmd "module list"
+
+cmd "which cmake"
+cmd "which mpirun"
+
+cmd "ctest -VV -R ductWedge"

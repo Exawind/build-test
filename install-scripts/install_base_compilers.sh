359277b38ac013542f76abbc25fba8b6370d3d35
Jon Rood <jon.rood@nrel.gov>
2018-11-22 06:56:30 -0700

    Updating rhodes compilers.yaml

--- install-scripts/install_base_compilers.sh
+++ install-scripts/install_base_compilers.sh
@@ -156,7 +156,6 @@ do
     cmd "spack install gcc@5.5.0 %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install gcc@4.9.4 %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install llvm %${COMPILER_NAME}@${COMPILER_VERSION}"
-    cmd "spack install flang %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install intel-parallel-studio@cluster.2019.1+advisor+inspector+mkl+mpi+vtune %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install intel-parallel-studio@cluster.2018.4+advisor+inspector+mkl+mpi+vtune %${COMPILER_NAME}@${COMPILER_VERSION}"
   fi

a78dc262b727e4e0878fa49045e3cbfbbef59cf4
Jon Rood <jon.rood@nrel.gov>
2018-11-27 10:51:34 -0700

    Add custom paraview.

--- configs/setup-spack.sh
+++ configs/setup-spack.sh
@@ -96,6 +96,7 @@ if [ "${MACHINE}" == 'peregrine' ] || \
   (set -x; cp -R custom-package-files/py-ipython ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
   (set -x; cp -R custom-package-files/py-traitlets ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
   (set -x; cp -R custom-package-files/nalu-wind ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
+  (set -x; cp -R custom-package-files/paraview ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
   (set -x; cp -R custom-package-files/trilinos-catalyst-ioss-adapter ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
 
   #Extra stuff for eagle

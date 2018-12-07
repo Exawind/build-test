1c3c20f9ec86aadc00b8c2771a1adb0b6705cce0
Jon Rood <jon.rood@nrel.gov>
2018-12-07 13:54:13 -0700

    Updating paraview custom package file.

--- configs/custom-package-files/paraview/package.py
+++ configs/custom-package-files/paraview/package.py
@@ -104,7 +104,7 @@ class Paraview(CMakePackage):
             run_env.prepend_path('LIBRARY_PATH', join_path(lib_dir,
                                  paraview_version))
             run_env.prepend_path('LD_LIBRARY_PATH', join_path(lib_dir,
-                             paraview_version))
+                                 paraview_version))
             if '+python' in self.spec:
                 run_env.prepend_path('PYTHONPATH', join_path(lib_dir,
                                      paraview_version))
@@ -113,9 +113,15 @@ class Paraview(CMakePackage):
                 run_env.prepend_path('PYTHONPATH', join_path(lib_dir,
                                      paraview_version, 'site-packages', 'vtk'))
         elif self.spec.satisfies('@5.5.0:'):
+            # Note the python2.7 directory likely needs to be smarter if the
+            # strict python 2:2.8 dependency goes away for Paraview in the
+            # future
             if '+python' in self.spec:
-                run_env.prepend_path('PYTHONPATH', join_path(lib_dir,
-                                     'python2.7', 'site-packages', 'vtkmodules'))
+                run_env.prepend_path('PYTHONPATH',
+                                     join_path(lib_dir,
+                                               'python2.7',
+                                               'site-packages',
+                                               'vtkmodules'))
 
     def cmake_args(self):
         """Populate cmake arguments for ParaView."""

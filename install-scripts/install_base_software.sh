3004c44822644977d0cb3e757501c9d7e17019f8
Jon Rood <jon.rood@nrel.gov>
2019-01-03 11:25:11 -0700

    Removing explicit paraview and petsc installs in base software script.

--- install-scripts/install_base_software.sh
+++ install-scripts/install_base_software.sh
@@ -57,7 +57,7 @@ case "${MYHOSTNAME}" in
   ;;
 esac
 
-DATE=2018-11-21
+DATE=2019-01-02
  
 if [ "${MACHINE}" == 'eagle' ]; then
   INSTALL_DIR=/nopt/nrel/ecom/hpacf/${TYPE}/${DATE}
@@ -251,11 +251,11 @@ do
     cmd "spack install percept %${COMPILER_NAME}@${COMPILER_VERSION} ^${TRILINOS_PERCEPT}@12.12.1 ^netcdf@4.3.3.1 ^hdf5@1.8.16 ^boost@1.60.0 ^parallel-netcdf@1.6.1"
     cmd "spack install masa %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install valgrind %${COMPILER_NAME}@${COMPILER_VERSION}"
-    cmd "spack install paraview+mpi+python+osmesa %${COMPILER_NAME}@${COMPILER_VERSION}"
+    #cmd "spack install paraview+mpi+python+osmesa %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install amrvis+mpi dims=3 %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install amrvis+mpi+profiling dims=2 %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install osu-micro-benchmarks %${COMPILER_NAME}@${COMPILER_VERSION}"
-    cmd "spack install petsc %${COMPILER_NAME}@${COMPILER_VERSION}"
+    #cmd "spack install petsc %${COMPILER_NAME}@${COMPILER_VERSION}"
   elif [ ${COMPILER_NAME} == 'intel' ]; then
     printf "\nInstalling Nalu-Wind stuff using ${COMPILER_NAME}@${COMPILER_VERSION}...\n"
     cmd "spack install --only dependencies nalu-wind+openfast+tioga+hypre+fftw %${COMPILER_NAME}@${COMPILER_VERSION} ^${TRILINOS}@${TRILINOS_BRANCH} ^intel-mpi ^intel-mkl"

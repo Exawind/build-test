c451e5b36589285fd60dee17b748631fd1562cd9
Jon Rood <jon.rood@nrel.gov>
2019-01-08 11:32:10 -0700

    Updating visit eagle script.

--- install-scripts/install_visit_eagle.sh
+++ install-scripts/install_visit_eagle.sh
@@ -15,8 +15,8 @@ cmd() {
 set -e
 
 cmd "module purge"
-cmd "module load gcc/7.3.0"
-cmd "module load python/2.7.15"
+cmd "module load gcc/4.9.4"
+cmd "module load python"
 cmd "module load git"
 cmd "module load binutils"
 cmd "module load libxml2/2.9.8-py2"
@@ -26,14 +26,19 @@ cmd "module load libx11"
 cmd "module load libxt"
 cmd "module load libsm"
 cmd "module load libice"
+cmd "module load bzip2"
 cmd "module list"
 
 cmd "mkdir -p /scratch/${USER}/.tmp"
 cmd "export TMPDIR=/scratch/${USER}/.tmp"
 
 export MY_VISIT_DIR=/nopt/nrel/ecom/hpacf/software/2019-01-02/visit/server
-export MY_PYTHON_LIB=${MY_VISIT_DIR}/visit/python/2.7.11/linux-x86_64_gcc-6.2/lib/libpython2.7.so
+export MY_PYTHON_LIB=${MY_VISIT_DIR}/visit/python/2.7.11/linux-x86_64_gcc-4.9/lib/libpython2.7.so
 
+# External mpi but you need an older gcc in the compiler wrappers to build certain things in the build_visit script
+#cmd "mkdir -p ${MY_VISIT_DIR} && cp build_visit2_13_2 ${MY_VISIT_DIR}/ && cd ${MY_VISIT_DIR} && export PAR_COMPILER=${OPENMPI_ROOT_DIR}/bin/mpicc && export PAR_COMPILER_CXX=${OPENMPI_ROOT_DIR}/bin/mpicxx && ./build_visit2_13_2 --makeflags -j36 --parallel --required --optional --all-io --nonio --no-fastbit --no-fastquery --prefix ${MY_VISIT_DIR}/install"
+
+# Use visit's own mpich
 cmd "mkdir -p ${MY_VISIT_DIR} && cp build_visit2_13_2 ${MY_VISIT_DIR}/ && cd ${MY_VISIT_DIR} && ./build_visit2_13_2 --makeflags -j36 --parallel --required --optional --all-io --nonio --no-fastbit --no-fastquery --prefix ${MY_VISIT_DIR}/install"
 
 #Then add this to /nopt/nrel/ecom/ecp/base/a/visit/install/2.13.0/bin/internallauncher

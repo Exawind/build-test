958b32537fd5de93689c63f6c7617d445f81841c
Jon Rood <jon.rood@nrel.gov>
2019-01-11 10:24:44 -0700

    Removing some custom packages.

--- configs/setup-spack.sh
+++ configs/setup-spack.sh
@@ -91,9 +91,6 @@ if [ "${MACHINE}" == 'peregrine' ] || \
 
   #All machines do this
   (set -x; cp machines/${MACHINE}/*.yaml ${SPACK_ROOT}/etc/spack/)
-  (set -x; cp -R custom-package-files/py-numpy ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-  #(set -x; cp -R custom-package-files/py-ipython ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-  #(set -x; cp -R custom-package-files/py-traitlets ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
   (set -x; cp -R custom-package-files/nalu-wind ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
   (set -x; cp -R custom-package-files/trilinos-catalyst-ioss-adapter ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
   (set -x; cp -R custom-package-files/tioga ${SPACK_ROOT}/var/spack/repos/builtin/packages/)

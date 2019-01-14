dcdfddb65c7a299eef3ee1fb8ca6a63d1bf74049
Jon Rood <jon.rood@nrel.gov>
2019-01-14 15:30:00 -0700

    Adding PGI 18.10 to compilers.yaml.software.

--- install-scripts/install_visit_eagle.sh
+++ install-scripts/install_visit_eagle.sh
@@ -32,7 +32,7 @@ cmd "module list"
 cmd "mkdir -p /scratch/${USER}/.tmp"
 cmd "export TMPDIR=/scratch/${USER}/.tmp"
 
-export MY_VISIT_DIR=/nopt/nrel/ecom/hpacf/software/2019-01-02/visit/server
+export MY_VISIT_DIR=/nopt/nrel/ecom/hpacf/software/2019-01-10/visit/server
 export MY_PYTHON_LIB=${MY_VISIT_DIR}/visit/python/2.7.11/linux-x86_64_gcc-4.9/lib/libpython2.7.so
 
 # External mpi but you need an older gcc in the compiler wrappers to build certain things in the build_visit script
@@ -41,12 +41,15 @@ export MY_PYTHON_LIB=${MY_VISIT_DIR}/visit/python/2.7.11/linux-x86_64_gcc-4.9/li
 # Use visit's own mpich
 cmd "mkdir -p ${MY_VISIT_DIR} && cp build_visit2_13_2 ${MY_VISIT_DIR}/ && cd ${MY_VISIT_DIR} && ./build_visit2_13_2 --makeflags -j36 --parallel --required --optional --all-io --nonio --no-fastbit --no-fastquery --prefix ${MY_VISIT_DIR}/install"
 
-#Then add this to /nopt/nrel/ecom/hpacf/software/2019-01-02/visit/server/install/2.13.0/bin/internallauncher
+# Visit requires telling numpy to overwrite some files during installation
+# Visit also requires copying some .bz2 files from nektar++/ThirdParty directory
+
+#Then add this to /nopt/nrel/ecom/hpacf/software/2019-01-10/visit/server/install/2.13.2/bin/internallauncher
 ##execfile('/nopt/Modules/default/init/python.py')
 ##module('purge')
 ##module('load gcc/4.9.4')
-#SETENV("PATH", self.joinpaths(["/nopt/nrel/ecom/hpacf/software/2019-01-02/visit/server/install/bin", "/nopt/nrel/ecom/hpacf/software/2019-01-02/visit/server/install/2.13.2/linux-x86_64/bin", GETENV("PATH")]))
-#SETENV("LD_LIBRARY_PATH", self.joinpaths(["/nopt/nrel/ecom/hpacf/software/2019-01-02/visit/server/install/2.13.2/linux-x86_64/lib", GETENV("LD_LIBRARY_PATH")]))
-#SETENV("LD_LIBRARY_PATH", self.joinpaths(["/nopt/nrel/ecom/hpacf/software/2019-01-02/spack/opt/spack/linux-centos7-x86_64/gcc-7.3.0/netlib-lapack-3.8.0-e4xwkqx3xg7gdlz3rkoczxfcpqwkcvtw/lib64", GETENV("LD_LIBRARY_PATH")]))
-#SETENV("LIBRARY_PATH", self.joinpaths(["/nopt/nrel/ecom/hpacf/software/2019-01-02/visit/server/install/2.13.2/linux-x86_64/lib", GETENV("LIBRARY_PATH")]))
-#SETENV("VISIT_ROOT_DIR", "/nopt/nrel/ecom/hpacf/software/2019-01-02/visit/server/install")
+#SETENV("PATH", self.joinpaths(["/nopt/nrel/ecom/hpacf/software/2019-01-10/visit/server/install/bin", "/nopt/nrel/ecom/hpacf/software/2019-01-10/visit/server/install/2.13.2/linux-x86_64/bin", GETENV("PATH")]))
+#SETENV("LD_LIBRARY_PATH", self.joinpaths(["/nopt/nrel/ecom/hpacf/software/2019-01-10/visit/server/install/2.13.2/linux-x86_64/lib", GETENV("LD_LIBRARY_PATH")]))
+#SETENV("LD_LIBRARY_PATH", self.joinpaths(["/nopt/nrel/ecom/hpacf/software/2019-01-10/spack/opt/spack/linux-centos7-x86_64/gcc-7.3.0/netlib-lapack-3.8.0-e4xwkqx3xg7gdlz3rkoczxfcpqwkcvtw/lib64", GETENV("LD_LIBRARY_PATH")]))
+#SETENV("LIBRARY_PATH", self.joinpaths(["/nopt/nrel/ecom/hpacf/software/2019-01-10/visit/server/install/2.13.2/linux-x86_64/lib", GETENV("LIBRARY_PATH")]))
+#SETENV("VISIT_ROOT_DIR", "/nopt/nrel/ecom/hpacf/software/2019-01-10/visit/server/install")

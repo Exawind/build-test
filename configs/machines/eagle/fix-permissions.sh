39ff27951e4d422ed6a841a752b73edec009fca4
Jon Rood <jon.rood@nrel.gov>
2019-01-23 20:41:07 -0700

    Reducing priority to minimum in fix-permissions script.

--- configs/machines/eagle/fix-permissions.sh
+++ configs/machines/eagle/fix-permissions.sh
@@ -8,5 +8,5 @@ cmd() {
   if ${execute_cmds}; then eval "$@"; fi
 }
 
-cmd "chmod -R a+rX,go-w /nopt/nrel/ecom/hpacf"
-cmd "chgrp -R n-ecom /nopt/nrel/ecom/hpacf"
+cmd "nice -n 19 ionice -c 3 chmod -R a+rX,go-w /nopt/nrel/ecom/hpacf"
+cmd "nice -n 19 ionice -c 3 chgrp -R n-ecom /nopt/nrel/ecom/hpacf"

5e029e94211147da90913078063f70bb68da73d8
Jon Rood <jon.rood@nrel.gov>
2019-02-06 11:46:37 -0700

    Removing custom paraview package.

--- configs/setup-spack.sh
+++ configs/setup-spack.sh
@@ -92,7 +92,6 @@ if [ "${MACHINE}" == 'peregrine' ] || \
   #All machines do this
   (set -x; cp machines/${MACHINE}/*.yaml ${SPACK_ROOT}/etc/spack/)
   (set -x; cp custom-package-files/trilinos/package.py ${SPACK_ROOT}/var/spack/repos/builtin/packages/trilinos/package.py)
-  (set -x; cp custom-package-files/paraview/package.py ${SPACK_ROOT}/var/spack/repos/builtin/packages/paraview/package.py)
 
   #Extra stuff for eagle
   if [ ${MACHINE} == 'eagle' ]; then

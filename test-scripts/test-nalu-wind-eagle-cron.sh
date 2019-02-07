44d30756c9c0856ae764a65b4ca95597b7397ec8
Jon Rood <jon.rood@nrel.gov>
2019-02-07 16:14:41 -0700

    Updating cron script for eagle.

--- test-scripts/test-nalu-wind-eagle-cron.sh
+++ test-scripts/test-nalu-wind-eagle-cron.sh
@@ -5,10 +5,11 @@
 cd /projects/hfm/exawind/nalu-wind-testing/logs && \
 sbatch \
 -J test-nalu-wind \
--N 1
+-N 1 \
 -t 4:00:00 \
 -A hfm \
 -p standard \
--o %x.o%j \
+-o "%x.o%j" \
+--gres=gpu:2 \
 /projects/hfm/exawind/nalu-wind-testing/build-test/test-scripts/test-nalu-wind.sh
 

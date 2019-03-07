3fdcd7389b78b952224fdc7b031ef3c7957d9026
Jon Rood <jon.rood@nrel.gov>
2019-03-07 15:23:18 -0700

    Add rsync to utilities list.

--- install-scripts/install_base_utilities.sh
+++ install-scripts/install_base_utilities.sh
@@ -161,6 +161,7 @@ do
     cmd "spack install strace %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install ninja@kitware %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install gdb %${COMPILER_NAME}@${COMPILER_VERSION}"
+    cmd "spack install rsync %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack load texinfo %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack load texlive %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack load flex %${COMPILER_NAME}@${COMPILER_VERSION}"

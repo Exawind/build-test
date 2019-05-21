538beb322fdc18cfc5f04c51861215c3ab074109
Jon Rood <jon.rood@nrel.gov>
2019-05-21 13:45:33 -0600

    Trying to use the ridiculous relative path to the Yaml-CPP impl.h header for the address sanitizer since that's the one it reports and the regular absolute path didn't work.

--- test-scripts/test-nalu-wind.sh
+++ test-scripts/test-nalu-wind.sh
@@ -323,7 +323,8 @@ test_configuration() {
     printf "\nSetting up address sanitizer in Clang...\n"
     # Create blacklist for suppressing impl.h file in Yaml-CPP library
     printf "\nWriting source files to blacklist file...\n"
-    (set -x; printf "src:$(spack location -i yaml-cpp %${COMPILER_ID})/include/yaml-cpp/node/impl.h" > ${NALU_WIND_DIR}/build/asan_blacklist.txt)
+    #(set -x; printf "src:$(spack location -i yaml-cpp %${COMPILER_ID})/include/yaml-cpp/node/impl.h" > ${NALU_WIND_DIR}/build/asan_blacklist.txt)
+    (set -x; printf "src:$(spack location -i yaml-cpp %${COMPILER_ID})/lib/cmake/yaml-cpp/../../../include/yaml-cpp/node/impl.h" > ${NALU_WIND_DIR}/build/asan_blacklist.txt)
     export CXXFLAGS="-fsanitize=address -fno-omit-frame-pointer -fsanitize-blacklist=${NALU_WIND_DIR}/build/asan_blacklist.txt"
     printf "export CXXFLAGS=${CXXFLAGS}\n"
     # Probably should try to solve the Nalu-Wind container overflow errors sometime, but we currently ignore them

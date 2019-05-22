d8bd520fdefc702ad3d3a52a190d486781a60029
Jon Rood <jon.rood@nrel.gov>
2019-05-22 16:16:26 -0600

    Updating utilities script.

--- install-scripts/install_base_utilities.sh
+++ install-scripts/install_base_utilities.sh
@@ -96,7 +96,7 @@ do
     cmd "module use /nopt/nrel/ecom/hpacf/utilities/modules"
     cmd "module use /nopt/nrel/ecom/hpacf/compilers/modules"
     cmd "module load git"
-    cmd "module load python/2.7.15"
+    cmd "module load python"
     cmd "module load curl"
     cmd "module list"
     printf "\nMaking and setting TMPDIR to disk...\n"
@@ -117,7 +117,7 @@ do
     cmd "module load bison"
     cmd "module load wget"
     cmd "module load bc"
-    cmd "module load python/2.7.15"
+    cmd "module load python"
     cmd "module list"
     cmd "source ${SPACK_ROOT}/share/spack/setup-env.sh"
   fi
@@ -150,7 +150,6 @@ do
     cmd "spack install htop %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install makedepend %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install cppcheck %${COMPILER_NAME}@${COMPILER_VERSION}"
-    cmd "spack install likwid %${COMPILER_NAME}@${COMPILER_VERSION}"
     cmd "spack install texinfo %${COMPILER_NAME}@${COMPILER_VERSION}"
     # Remove gtkplus dependency from ghostscript
     cmd "spack install image-magick %${COMPILER_NAME}@${COMPILER_VERSION}"
@@ -168,6 +167,9 @@ do
     cmd "spack install flex@2.5.39 %${COMPILER_NAME}@${COMPILER_VERSION}"
     (set -x; spack install gnutls %${COMPILER_NAME}@${COMPILER_VERSION} ^/$(spack find -L autoconf %${COMPILER_NAME}@${COMPILER_VERSION} | grep autoconf | awk -F" " '{print $1}' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"))
     cmd "spack install gnuplot %${COMPILER_NAME}@${COMPILER_VERSION}"
+    if [ "${MACHINE}" != 'rhodes' ]; then
+      cmd "spack install likwid %${COMPILER_NAME}@${COMPILER_VERSION}"
+    fi
   fi
 
   cmd "unset TMPDIR"

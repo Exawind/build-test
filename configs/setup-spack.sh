6e3bf8cb6665699f20796c05032d2c039ba13d15
Jon Rood <jon.rood@nrel.gov>
2019-05-28 16:57:58 -0600

    Removing a bunch of older machine stuff.

--- configs/setup-spack.sh
+++ configs/setup-spack.sh
@@ -14,7 +14,6 @@ OS=$(uname -s)
 
 #Use kind of ridiculous logic to find the machine name
 if [ ${OS} == 'Darwin' ]; then
-  MACHINE=mac
   OSX=$(sw_vers -productVersion)
   case "${OSX}" in
     10.12*)
@@ -25,65 +24,22 @@ if [ ${OS} == 'Darwin' ]; then
     ;;
   esac
 elif [ ${OS} == 'Linux' ]; then
-  case "${NERSC_HOST}" in
-    cori)
-      MACHINE=cori
+  case "${NREL_CLUSTER}" in
+    eagle)
+      MACHINE=eagle
     ;;
-    "")
-      if [ -f "/etc/nrel" ]; then 
-        MACHINE=merlin
-      else
-        case "${NREL_CLUSTER}" in
-          eagle)
-            MACHINE=eagle
-          ;;
-          peregrine)
-            MACHINE=peregrine
-          ;;
-        esac
-        MYHOSTNAME=$(hostname -d)
-        case "${MYHOSTNAME}" in
-          mcp.alcf.anl.gov)
-            MACHINE=mira
-          ;;
-          fst.alcf.anl.gov)
-            MACHINE=mira
-          ;;
-          ices.utexas.edu)
-            MACHINE=ices
-          ;;
-        esac
-        MYHOSTNAME=$(hostname)
-        case "${MYHOSTNAME}" in
-          mutrino)
-            MACHINE=mutrino
-          ;;
-          theta*)
-            MACHINE=theta
-          ;;
-        esac
-        MYHOSTNAME=$(hostname -s)
-        case "${MYHOSTNAME}" in
-          rhodes)
-            MACHINE=rhodes
-          ;;
-        esac
-      fi
+  esac
+  MYHOSTNAME=$(hostname -s)
+  case "${MYHOSTNAME}" in
+    rhodes)
+      MACHINE=rhodes
     ;;
   esac
 fi
 
 # Copy machine-specific configuration for Spack if we recognize the machine
-if [ "${MACHINE}" == 'peregrine' ] || \
-   [ "${MACHINE}" == 'eagle' ] || \
+if [ "${MACHINE}" == 'eagle' ] || \
    [ "${MACHINE}" == 'rhodes' ] || \
-   [ "${MACHINE}" == 'merlin' ] || \
-   [ "${MACHINE}" == 'cori' ] || \
-   [ "${MACHINE}" == 'mira' ] || \
-   [ "${MACHINE}" == 'theta' ] || \
-   [ "${MACHINE}" == 'mutrino' ] || \
-   [ "${MACHINE}" == 'ices' ] || \
-   [ "${MACHINE}" == 'mac' ] || \
    [ "${MACHINE}" == 'mac_sierra' ] || \
    [ "${MACHINE}" == 'mac_high_sierra' ]; then
 
@@ -103,66 +59,26 @@ if [ "${MACHINE}" == 'peregrine' ] || \
     (set -x; cp machines/${MACHINE}/modules.yaml.software ${SPACK_ROOT}/etc/spack/modules.yaml)
   fi
 
-  #Extra stuff for peregrine
-  if [ ${MACHINE} == 'peregrine' ]; then
-    (set -x; cp machines/${MACHINE}/compilers.yaml.software ${SPACK_ROOT}/etc/spack/compilers.yaml)
-    (set -x; cp machines/${MACHINE}/modules.yaml.software ${SPACK_ROOT}/etc/spack/modules.yaml)
-  fi
-
   #Extra stuff for rhodes
   if [ ${MACHINE} == 'rhodes' ]; then
     (set -x; cp machines/${MACHINE}/compilers.yaml.software ${SPACK_ROOT}/etc/spack/compilers.yaml)
     (set -x; cp machines/${MACHINE}/modules.yaml.software ${SPACK_ROOT}/etc/spack/modules.yaml)
   fi
 
-  #Extra stuff for merlin
-  #if [ ${MACHINE} == 'merlin' ]; then
-    #nothing at the moment
-  #fi
-
-  #Extra stuff for cori
-  #if [ ${MACHINE} == 'cori' ]; then
-    #nothing at the moment
-  #fi
-
-  #Extra stuff for mutrino
-  if [ ${MACHINE} == 'mutrino' ]; then
-    (set -x; cp -R machines/${MACHINE}/netcdf ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-  fi
-
   #Extra stuff for mira
-  if [ ${MACHINE} == 'mira' ]; then
-    (set -x; cp -R machines/${MACHINE}/libsigsegv ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-    (set -x; cp -R machines/${MACHINE}/nalu ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-    (set -x; cp -R machines/${MACHINE}/nalu-wind ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-    (set -x; cp -R machines/${MACHINE}/netcdf ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-    (set -x; cp -R machines/${MACHINE}/parallel-netcdf ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-    (set -x; cp -R machines/${MACHINE}/superlu ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-    (set -x; cp -R machines/${MACHINE}/trilinos ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-    (set -x; cp -R machines/${MACHINE}/yaml-cpp ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-  fi
-
-  #Extra stuff for theta
-  # SuperLU tries to run a KNL executable on the haswell login nodes without this custom SuperLU version
-  if [ ${MACHINE} == 'theta' ]; then
-    (set -x; cp -R machines/${MACHINE}/superlu ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
-  fi
-
-  #Extra stuff for ices
-  #if [ ${MACHINE} == 'ices' ]; then
-    #nothing at the moment
+  #if [ ${MACHINE} == 'mira' ]; then
+  #  (set -x; cp -R machines/${MACHINE}/libsigsegv ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
+  #  (set -x; cp -R machines/${MACHINE}/nalu ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
+  #  (set -x; cp -R machines/${MACHINE}/nalu-wind ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
+  #  (set -x; cp -R machines/${MACHINE}/netcdf ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
+  #  (set -x; cp -R machines/${MACHINE}/parallel-netcdf ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
+  #  (set -x; cp -R machines/${MACHINE}/superlu ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
+  #  (set -x; cp -R machines/${MACHINE}/trilinos ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
+  #  (set -x; cp -R machines/${MACHINE}/yaml-cpp ${SPACK_ROOT}/var/spack/repos/builtin/packages/)
   #fi
 
-  #Extra stuff for macs
-  #if [ ${MACHINE} == 'mac' ] || [ "${MACHINE}" == 'mac_sierra' ] || [ "${MACHINE}" == 'mac_high_sierra' ]; then
+  #if [ "${MACHINE}" == 'mac_sierra' ] || [ "${MACHINE}" == 'mac_high_sierra' ]; then
     #nothing at the moment
-  #fi
-
-  #Use branch instead of tag so spack will checkout a real git repo instead of caching a tar.gz of a branch
-  #if [ ${MACHINE} == 'mac' ] || [ "${MACHINE}" == 'mac_sierra' ] || [ "${MACHINE}" == 'mac_high_sierra' ]; then
-  #  (set -x; sed -i "" -e "s/tag=/branch=/g" ${SPACK_ROOT}/var/spack/repos/builtin/packages/trilinos/package.py)
-  #else
-  #  (set -x; sed -i "s/tag=/branch=/g" ${SPACK_ROOT}/var/spack/repos/builtin/packages/trilinos/package.py)
   #fi
 else
   printf "\nMachine name not found.\n"

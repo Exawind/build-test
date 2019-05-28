8a852a8b5cbad7f0bef477db4a5d5482837f5915
Jon Rood <jon.rood@nrel.gov>
2019-05-28 10:41:13 -0600

    Removing machine-specific cron scripts.

--- test-scripts/test-nalu-wind-mac-cron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-
-#Script that runs the nightly Nalu-Wind tests at NREL on a Mac
-
-set -e
-
-cd /Users/jrood/nalu-wind-testing/logs && \
-/Users/jrood/nalu-wind-testing/build-test/test-scripts/test-nalu-wind.sh &> "test-nalu-wind-$(date +%Y-%m-%d).log"

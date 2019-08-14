5f488c8b645fb440e673ce4f4c8e35848e39e63d
Jon Rood <jon.rood@nrel.gov>
2019-08-14 09:03:45 -0600

    Add rst norms into update_golds script.

--- configs/machines/rhodes/update_golds.sh
+++ configs/machines/rhodes/update_golds.sh
@@ -5,11 +5,12 @@ cmd() {
   eval "$@"
 }
 
-TEST_DIR=${HOME}/exawind/test
+TEST_DIR=${HOME}/gold
 GOLD_DIR=${HOME}/exawind/nalu-wind/reg_tests/test_files
 
-TESTS=( elemClosedDomain ablUnstableEdge_ra ablUnstableEdge elemBackStepLRSST ablNeutralEdgeSegregated ablStableElem dgNonConformalFluidsEdge ablNeutralEdge heatedWaterChannelEdge movingCylinder airfoilRANSEdge milestoneRun ablHill3d_pp airfoilRANSElem fluidsPmrChtPeriodic heliumPlume uqSlidingMeshDG )
+TESTS=( ablNeutralEdge ablNeutralEdgeSegregated ablStableElem ablUnstableEdge ablUnstableEdge_ra airfoilRANSEdge airfoilRANSElem elemBackStepLRSST elemClosedDomain heatedWaterChannelEdge heliumPlume milestoneRun movingCylinder uqSlidingMeshDG waleElemXflowMixFrac3.5m )
 
 for TEST in "${TESTS[@]}"; do
   cmd "cp ${TEST_DIR}/${TEST}/${TEST}.norm ${GOLD_DIR}/${TEST}/${TEST}.norm.gold"
+  cmd "cp ${TEST_DIR}/${TEST}/${TEST}_rst.norm ${GOLD_DIR}/${TEST}/${TEST}_rst.norm.gold"
 done

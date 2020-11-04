183a64f5dfe318016a466b9441a50e7caa9270ae
Jon Rood <jon.rood@nrel.gov>
2020-11-04 09:36:45 -0700

    Update gold update script.

--- configs/machines/rhodes/update_golds.sh
+++ configs/machines/rhodes/update_golds.sh
@@ -5,12 +5,14 @@ cmd() {
   eval "$@"
 }
 
-TEST_DIR=${HOME}/gold
+TEST_DIR=${HOME}/exawind/golds
 GOLD_DIR=${HOME}/exawind/nalu-wind/reg_tests/test_files
 
-TESTS=( ablNeutralEdge ablNeutralEdgeSegregated ablStableElem ablUnstableEdge ablUnstableEdge_ra airfoilRANSEdge airfoilRANSElem elemBackStepLRSST elemClosedDomain heatedWaterChannelEdge heliumPlume milestoneRun movingCylinder uqSlidingMeshDG waleElemXflowMixFrac3.5m )
+TESTS=( ablNeutralStat )
 
 for TEST in "${TESTS[@]}"; do
   cmd "cp ${TEST_DIR}/${TEST}/${TEST}.norm ${GOLD_DIR}/${TEST}/${TEST}.norm.gold"
   cmd "cp ${TEST_DIR}/${TEST}/${TEST}_rst.norm ${GOLD_DIR}/${TEST}/${TEST}_rst.norm.gold"
+  cmd "cp ${TEST_DIR}/${TEST}/${TEST}_Input.norm ${GOLD_DIR}/${TEST}/${TEST}_Input.norm.gold"
+  cmd "cp ${TEST_DIR}/${TEST}/${TEST}.nc ${GOLD_DIR}/${TEST}/${TEST}.nc.gold"
 done

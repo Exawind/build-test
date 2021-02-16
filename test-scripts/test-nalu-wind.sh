8a0fd7b9d984b689c521e37e575f8fc42ceb6127
Jon Rood <jon.rood@nrel.gov>
2021-02-16 14:09:11 -0700

    Don't uninstall hypre or openfast.

--- test-scripts/test-nalu-wind.sh
+++ test-scripts/test-nalu-wind.sh
@@ -127,12 +127,12 @@ test_configuration() {
   # Uninstall packages we want to track; it's an error if they don't exist yet, but a soft error
   printf "\nUninstalling Trilinos (this is fine to error when tests are first run or building Trilinos has previously failed)...\n"
   cmd "spack uninstall -a -y trilinos@${TRILINOS_BRANCH} %${COMPILER_ID} || true"
-  printf "\nUninstalling OpenFAST (this is fine to error when tests are first run or building OpenFAST has previously failed)...\n"
-  cmd "spack uninstall -a -y openfast %${COMPILER_ID} || true"
+  #printf "\nUninstalling OpenFAST (this is fine to error when tests are first run or building OpenFAST has previously failed)...\n"
+  #cmd "spack uninstall -a -y openfast %${COMPILER_ID} || true"
   #printf "\nUninstalling TIOGA (this is fine to error when tests are first run or building TIOGA has previously failed)...\n"
   #cmd "spack uninstall -a -y tioga %${COMPILER_ID} || true"
-  printf "\nUninstalling hypre...\n"
-  cmd "spack uninstall -a -y hypre %${COMPILER_ID} || true"
+  #printf "\nUninstalling hypre...\n"
+  #cmd "spack uninstall -a -y hypre %${COMPILER_ID} || true"
 
   # Update packages we want to track; it's an error if they don't exist yet, but a soft error
   printf "\nUpdating and cleaning Trilinos stage directory (this is fine to error when tests are first run)...\n"

fc325ff777e7762eabebfaff18534ae6ea605a95
Shreyas Ananthan <shreyas@umd.edu>
2021-03-30 07:39:40 -0600

    Set test tolerance explicitly for GCC tests
    Pass `-DTEST_REL_TOL` and `-DTEST_TOLERANCE` for GCC tests. 
    
    Since we only have one GCC compiler version in the test suite now, we add this without checking for the version.

--- test-scripts/test-nalu-wind.sh
+++ test-scripts/test-nalu-wind.sh
@@ -348,6 +348,8 @@ test_configuration() {
   if [ "${MACHINE_NAME}" == 'eagle' ]; then
     export CXXFLAGS="-Xcudafe --diag_suppress=code_is_unreachable -Wimplicit-fallthrough=0"
     CMAKE_CONFIGURE_ARGS="-DTEST_REL_TOL=1.0e-7 -DTEST_TOLERANCE:STRING=1.0e-8 ${CMAKE_CONFIGURE_ARGS}"
+  elif [ "${COMPILER_NAME}" == 'gcc' ]; then
+    CMAKE_CONFIGURE_ARGS="-DTEST_REL_TOL:STRING=1.0e-13 -DTEST_TOLERANCE:STRING=1.0e-15 ${CMAKE_CONFIGURE_ARGS}"
   elif [ "${MACHINE_NAME}" == 'mac' ]; then
     CMAKE_CONFIGURE_ARGS="-DTEST_TOLERANCE:STRING=100000.0 ${CMAKE_CONFIGURE_ARGS}"
   fi
